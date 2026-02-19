ce1323f90541924b221592b098f90dbb0eb4aa17
late <lucky>
2026-02-20 01:06:49 +0300

    🍪 60 kurabiye görseli .png -> .webp düzeltildi

--- lib/widgets/bento_grid.dart
+++ lib/widgets/bento_grid.dart
@@ -116,7 +116,7 @@ class _BentoGridState extends State<BentoGrid>
                               child: Transform.rotate(
                                 angle: -0.08,
                                 child: Image.asset(
-                                  'assets/images/tarot/tarotbuton_büyüktarot.webp',
+                                  'assets/images/tarot/tarotbuton_büyüktarot.webp',
                                   width: 258,
                                   height: 258,
                                   fit: BoxFit.contain,
