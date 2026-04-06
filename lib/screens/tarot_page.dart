00cd0a8bd8c05a024290934eb7459e3fde418878
Sadam <victor7846466@gmail.com>
2026-04-06 22:29:05 +0300

    UI: Proportionally shrunk the fixed height constraint of all 4 modal panels from 460px to 400px to perfectly frame the inner content and eliminate awkward top/bottom negative space

--- lib/screens/tarot_page.dart
+++ lib/screens/tarot_page.dart
@@ -3320,7 +3320,7 @@ class _TarotPageState extends State<TarotPage> with TickerProviderStateMixin {
                   filter: ui.ImageFilter.blur(sigmaX: 24, sigmaY: 24),
                   child: Container(
                     padding: const EdgeInsets.all(24),
-                    height: 460,
+                    height: 400,
                     alignment: Alignment.center,
                     decoration: BoxDecoration(
                       color: _isPremiumUser
@@ -3572,7 +3572,7 @@ class _TarotPageState extends State<TarotPage> with TickerProviderStateMixin {
                   filter: ui.ImageFilter.blur(sigmaX: 24, sigmaY: 24),
                   child: Container(
                     padding: const EdgeInsets.all(24),
-                    height: 460,
+                    height: 400,
                     alignment: Alignment.center,
                     decoration: BoxDecoration(
                       color: _isPremiumUser
